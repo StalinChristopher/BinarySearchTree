73c96d8fa97c1d7cd4221d91446eb9262cd1c8b7
StalinChristopher <stalin.dsouza@ymedialabs.com>
2021-09-16 19:41:50 +0530

    added method to add elements into the binary tree recursively

---
+++ src/com/yml/binarysearchtree/MyBinaryNode.java
@@ -0,0 +1,37 @@
+package com.yml.binarysearchtree;
+
+public class MyBinaryNode<K extends Comparable<K>> {
+	K key;
+    MyBinaryNode<K> left;
+    MyBinaryNode<K> right;
+
+    public K getKey() {
+        return key;
+    }
+
+    public void setKey(K key) {
+        this.key = key;
+    }
+
+    public MyBinaryNode<K> getLeft() {
+        return left;
+    }
+
+    public void setLeft(MyBinaryNode<K> left) {
+        this.left = left;
+    }
+
+    public MyBinaryNode<K> getRight() {
+        return right;
+    }
+
+    public void setRight(MyBinaryNode<K> right) {
+        this.right = right;
+    }
+
+    MyBinaryNode(K key){
+        this.key = key;
+        this.left = null;
+        this.right = null;
+    }
+}
---
+++ src/com/yml/binarysearchtree/MyBinarySearchTree.java
@@ -0,0 +1,39 @@
+package com.yml.binarysearchtree;
+
+public class MyBinarySearchTree<K extends Comparable<K>> {
+    private MyBinaryNode<K> root;
+
+
+    public void add(K key){
+        this.root = this.addRecursively(root, key);
+    }
+
+
+    private MyBinaryNode<K> addRecursively(MyBinaryNode<K> current, K key) {
+        if(current == null){
+            return new MyBinaryNode<>(key);
+        }
+        int compareResult = key.compareTo(current.key);
+        if( compareResult == 0) return current;
+        if( compareResult < 0 ){
+            current.left = addRecursively(current.left, key);
+        }
+        else{
+            current.right = addRecursively(current.right, key);
+        }  
+        return current;
+    }
+
+    public void printBinarySearchTree(){
+        this.printBinaryNode(root);
+    }
+
+    public void printBinaryNode(MyBinaryNode<K> temp){
+        if(temp == null)
+            return;
+        printBinaryNode(temp.left);
+        System.out.println(temp.getKey()+" ");
+        printBinaryNode(temp.right);
+    }
+    
+}
