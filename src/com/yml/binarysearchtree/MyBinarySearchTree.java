50187b959e0f03a3d03587cba9df4f42f2c51aa6
StalinChristopher <stalin.dsouza@ymedialabs.com>
2021-09-16 20:00:20 +0530

    Added comments to the MyBinarySearchTree class

--- src/com/yml/binarysearchtree/MyBinarySearchTree.java
+++ src/com/yml/binarysearchtree/MyBinarySearchTree.java
@@ -4,11 +4,24 @@ public class MyBinarySearchTree<K extends Comparable<K>> {
     private MyBinaryNode<K> root;
 
 
+    
+    /** 
+     * @param key
+     * external method to add a element into the binary search tree
+     */
     public void add(K key){
         this.root = this.addRecursively(root, key);
     }
 
 
+    
+    /** 
+     * @param current
+     * @param key
+     * @return MyBinaryNode<K>
+     * Internal method called inside add method to add the elements recursively in the binary search tree based on the value
+     * of the element
+     */
     private MyBinaryNode<K> addRecursively(MyBinaryNode<K> current, K key) {
         if(current == null){
             return new MyBinaryNode<>(key);
@@ -24,10 +37,18 @@ public class MyBinarySearchTree<K extends Comparable<K>> {
         return current;
     }
 
+    /**
+     * external method called in main class to print the entire binary search tree 
+     */
     public void printBinarySearchTree(){
         this.printBinaryNode(root);
     }
 
+    
+    /** 
+     * @param temp
+     * Method to print the nodes of the binary tree called internally
+     */
     public void printBinaryNode(MyBinaryNode<K> temp){
         if(temp == null)
             return;
@@ -36,11 +57,22 @@ public class MyBinarySearchTree<K extends Comparable<K>> {
         printBinaryNode(temp.right);
     }
 
+    
+    /** 
+     * @return int
+     * Method defined to get the size of the binary tree
+     */
     public int getSize() {
 		return this.getSizeRecursive(root);
 	}
 
-	private int getSizeRecursive(MyBinaryNode<K> curr) {
+	
+    /** 
+     * @param curr
+     * @return int
+     * Internal method called recursively to traverse through the tree and calculate the size
+     */
+    private int getSizeRecursive(MyBinaryNode<K> curr) {
 		return curr == null ? 0 : 1+getSizeRecursive(curr.left)+getSizeRecursive(curr.right);
 	}
     
